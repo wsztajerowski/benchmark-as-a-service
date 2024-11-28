2b4500d936bdae07edb32350e484337f2d95c866
Wiktor-Sztajerowski <wiktor.sztajerowski@gmail.com>
2024-11-28 23:00:34 +0100

    chore: add async options to jmh-with-async.sh

--- jmh-with-async.sh
+++ jmh-with-async.sh
@@ -1,18 +1,16 @@
 #!/bin/zsh
-
-sha=$(hexdump -n 4 -v -e '/1 "%02X"' /dev/urandom)
+#  event=<event>                    Event to sample: cpu, alloc, lock, wall, itimer;
+#                                   com.foo.Bar.methodName; any event from `perf list`
+#                                   e.g. cache-misses (default: [cpu])
+#  threads=<bool>                   Profile threads separately.
 
 AWS_PROFILE=localstack \
-AWS_ENDPOINT=https://s3.localhost.localstack.cloud:4566 \
 exec java -jar ./benchmark-runner/target/benchmark-runner.jar jmh-with-async \
---mongo-connection-string mongodb://localhost:27017/local_test \
---async-additional-param Key1=1 \
---async-additional-param Key2=2 \
---async-additional-param Key3=3 \
---s3-result-prefix=sePrefix \
---request-id=req1 \
+--async-additional-param event=wall \
+--async-additional-param threads=true \
 --benchmark-path=./benchmark-runner/target/jmh-benchmarks.jar \
---machine-readable-output ./benchmark-runner/target/jmh-results.json \
---process-output ./benchmark-runner/target/jmh-output.txt \
+--mongo-connection-string mongodb://localhost:27017/local_test \
+--s3-service-endpoint=https://s3.localhost.localstack.cloud:4566 \
+--s3-bucket baas \
  -wi 1 -f 1 -i 2 \
 "$@"
