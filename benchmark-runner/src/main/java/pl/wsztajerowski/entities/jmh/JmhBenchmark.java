f6c546a3953a175044cc03d782e6c88c0baee81e
Wiktor-Sztajerowski <wiktor.sztajerowski@gmail.com>
2024-12-21 22:48:36 +0100

    feat: separate jmh and async results

--- benchmark-runner/src/main/java/pl/wsztajerowski/entities/jmh/JmhBenchmark.java
+++ benchmark-runner/src/main/java/pl/wsztajerowski/entities/jmh/JmhBenchmark.java
@@ -2,14 +2,10 @@ package pl.wsztajerowski.entities.jmh;
 
 import dev.morphia.annotations.Entity;
 import dev.morphia.annotations.Id;
-
-import java.time.LocalDateTime;
 @Entity("jmh_benchmarks")
 public record JmhBenchmark(
     @Id
     JmhBenchmarkId benchmarkId,
     JmhResult jmhResult,
-    JmhResult jmhWithAsyncResult,
-    BenchmarkMetadata benchmarkMetadata,
-    LocalDateTime createdAt) {
+    BenchmarkMetadata benchmarkMetadata) {
 }
